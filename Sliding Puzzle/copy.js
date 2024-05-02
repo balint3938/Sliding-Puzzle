32093aecd14570b39b16b93a6efd3ca0bc6eaf30
balint3938 <167197306+balint3938@users.noreply.github.com>
2024-05-02 09:59:23 +0300

    Final?

--- Sliding Puzzle/copy.js
+++ Sliding Puzzle/copy.js
@@ -3,17 +3,21 @@ let tiles=document.querySelectorAll(".tile");
 let tileOrder=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
 const dir=[[-1,0],[0,1],[1,0],[0,-1]];
 let tileMatr=[[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]];
+let winComb=[[0,0,0,0,0,0],[0,1,2,3,4,0],[0,5,6,7,8,0],[0,9,10,11,12,0],[0,13,14,15,16,0],[0,0,0,0,0,0]];
 let activeTiles=[];
 let activeTilesNr=[];
 let vacantTile={vacantId: document.querySelector("#t16"),x:-1 ,y:-1};
 let removeFunc=[];
+let congrBox=document.createElement("div");
+let resetButton=document.createElement("button");
 
 startGame();
 
 function startGame(){  
-    durstenfeldShuffle(tileOrder);
+    do{
+       durstenfeldShuffle(tileOrder);
+    }while(countPolarity(tileOrder));
     for (let i=0;i<16;i++){
-        //tiles[i].addEventListener('click',slideTile);
         tiles[i].id="t"+tileOrder[i];
         if (tileOrder[i]===16){
             tiles[i].textContent='';
@@ -33,6 +37,23 @@ function startGame(){
         }
     }
     setMovableTiles();
+}
+
+function countPolarity(arrayTemp){
+    let inv=0;
+    for (let i=0;i<16;i++){
+        if (arrayTemp[i]!==16){
+            for (let j=i+1;j<16;j++){
+                if (arrayTemp[i]>arrayTemp[j] && arrayTemp[j]!==16){
+                    inv++;
+                }
+            }
+        }
+    }
+    if (inv%2===1){
+        return true;
+    }
+    return false;
 }
 
 function durstenfeldShuffle(array) {
@@ -74,7 +95,6 @@ function setMovableTiles(){
         activeTilesNr.push(3);
     }
     for (let i=0;i<activeTiles.length;i++){
-        activeTiles[i].style.background="red";
         activeTiles[i].addEventListener('click',removeFunc[i]=function dummy(){
             tileSlide(activeTilesNr[i],activeTiles[i]);
         });
@@ -104,12 +124,46 @@ function tileSlide(activeTilesNr,swappedTile){
 function tileReset(){
     for (let i=0;i<activeTiles.length;i++){
         activeTiles[i].removeEventListener('click', removeFunc[i]);
-        console.log(activeTiles[i]);
-        activeTiles[i].style.background="transparent";
     }
     while (activeTiles.length!==0){
         activeTiles.pop();
         activeTilesNr.pop();
     }
-    setMovableTiles();
+    if (matricesAreEqual(winComb,tileMatr)){
+        setMovableTiles();
+        gameWin();
+    }
+    else{
+        setMovableTiles();
+    }
+}
+
+function gameWin(){
+    congrBox.textContent="You win!";
+    congrBox.style.background='green';
+    resetButton.textContent="Reset";
+    resetButton.addEventListener('click',resetGame);
+    document.body.appendChild(congrBox);
+    document.body.appendChild(resetButton);
+}
+
+function resetGame(){
+    document.body.removeChild(congrBox);
+    document.body.removeChild(resetButton);
+    startGame();
+}
+
+function matricesAreEqual(matrix1, matrix2) {
+    if (matrix1.length !== matrix2.length || matrix1[0].length !== matrix2[0].length) {
+        return false;
+    }
+
+    for (let i = 0; i < matrix1.length; i++) {
+        for (let j = 0; j < matrix1[0].length; j++) {
+            if (matrix1[i][j] !== matrix2[i][j]) {
+                return false;
+            }
+        }
+    }
+    return true;
 }
